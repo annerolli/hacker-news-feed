509ade30702cb71df8881ac7110cc16735c860eb
Anastasia Tormozova <aatormozova@gmail.com>
2023-03-12 21:00:05 +0400

    refactor hacker news api types: use utility types

--- src/shared/services/hacker-news-api/hacker-news-api.types.ts
+++ src/shared/services/hacker-news-api/hacker-news-api.types.ts
@@ -53,19 +53,12 @@ export type TGetItemOptions = {
   itemId: TItemId;
 };
 
-export type TStory = TItem & {
-  type: 'story';
-  title: string;
-  text: string;
-  by: string;
-  time: number;
-  score: number;
-};
+export type TStory = TItem &
+  Required<Pick<TItem, 'title' | 'text' | 'by' | 'time' | 'score'>> & {
+    type: 'story';
+  };
 
-export type TComment = TItem & {
-  type: 'comment';
-  text: string;
-  by: string;
-  time: number;
-  parent: TItemId;
-};
+export type TComment = TItem &
+  Required<Pick<TItem, 'text' | 'by' | 'time' | 'parent'>> & {
+    type: 'comment';
+  };
